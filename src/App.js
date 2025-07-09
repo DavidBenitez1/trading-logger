537418dfb0ec6135a153ab54c12d1225f7b014fb
DavidBenitez1 <jndavidbenitez@gmail.com>
2025-07-09 15:06:45 -0300

    App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TradeLogger from "./TradeLogger";
+
+function App() {
+  return (
+    <div className="App">
+      <TradeLogger />
+    </div>
+  );
+}
+
+export default App;
